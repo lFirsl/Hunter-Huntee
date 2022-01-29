ea14d471a7d7c2b2e25d971810936349407b3e73
Stormyrice <56242638+Stormyrice@users.noreply.github.com>
2022-01-29 11:22:36 +0000

    Added enemyScript

--- Assets/HxH/enemyScript.cs
+++ Assets/HxH/enemyScript.cs
@@ -3,20 +3,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(BoxCollider))]
 public class enemyScript : MonoBehaviour
 {
-
+    public float health;
+    public float movementSpeed;
+    public float attackInterval;
     public float damage = 20f;
+    private float currentCooldown;
+    
+    public playScript playerScript;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentCooldown = attackInterval;
+        playScript playerScript = GameObject.Find("Player").GetComponent<playScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (currentCooldown <= 0)
+        {
+            Attack();
+            currentCooldown = attackInterval;
+        }
+        else
+        {
+            currentCooldown -= Time.deltaTime;
+        }
         
+        
+        
+    }
+
+    public virtual void Attack()
+    {
+        currentCooldown = attackInterval;
     }
 
     private void OnCollisionEnter(Collision coll)
---
+++ Assets/HxH/enemy_Spike.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_Spike : enemyScript
+{
+
+    public enemy_Spike()
+    {
+        base.damage = 20f;
+        base.health = 100f;
+        base.attackInterval = 20f;
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void Attack()
+    {
+        
+    }
+}
