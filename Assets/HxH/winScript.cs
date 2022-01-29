bccf653a9068da88f02314a5a85d10115c553de4
Nicholas Stafie <10801854+nstafie@users.noreply.github.com>
2022-01-29 21:22:13 +0000

    feat: emit stat update POST to server

--- Assets/HxH/winScript.cs
+++ Assets/HxH/winScript.cs
@@ -1,8 +1,22 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
+using Newtonsoft.Json;
+
+
+public class StatUpdate
+{
+    public int EnemiesKilled { get; set; }
+    public int Deaths { get; set; }
+    public int DamageTaken { get; set; }
+    public int LevelsFinished { get; set; }
+    public int CharacterSwitches { get; set; }
+}
+
 
 [RequireComponent(typeof(Rigidbody))]
 [RequireComponent(typeof(BoxCollider))]
@@ -10,7 +24,7 @@ public class winScript : MonoBehaviour
 {
     public Rigidbody rb;
     public BoxCollider bc;
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,7 +36,41 @@ public class winScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void UploadStats()
+    {
+        StatUpdate stat_update = new StatUpdate {
+            EnemiesKilled = 6,
+            Deaths = 9,
+            DamageTaken = 4,
+            LevelsFinished = 2,
+            CharacterSwitches = 0,
+        };
+        string data = JsonConvert.SerializeObject(stat_update);
+        Debug.Log(data);
+        byte[] bytes = Encoding.UTF8.GetBytes(data);
+
+
+
+        using (UnityWebRequest www = new UnityWebRequest("http://127.0.0.1:9000/update", "POST"))
+        {
+            www.uploadHandler = (UploadHandler) new UploadHandlerRaw(bytes);
+            www.downloadHandler = (DownloadHandler) new DownloadHandlerBuffer();
+            www.SetRequestHeader("Content-Type", "application/json");
+
+            www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.ConnectionError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log(www.downloadHandler.text);
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -30,6 +78,7 @@ public class winScript : MonoBehaviour
         if (other.GetComponent<playScript>())
         {
             Debug.Log("COLLIDED");
+            UploadStats();
             if (GameObject.Find("LevelLoader"))
             {
                 GameObject ob = GameObject.Find("LevelLoader");
