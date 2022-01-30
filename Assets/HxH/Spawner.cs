02b79dd14b747b07b7d32fcc167b8cb5494df2b9
kenlo256 <68675014+kenlo256@users.noreply.github.com>
2022-01-30 01:34:26 +0000

    spawner added

---
+++ Assets/HxH/Spawner.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] enemies;
+    public Vector3 spawnLimit;
+    
+    public float spawnWait;
+    public float spawnMinWait;
+    public float spawnMaxWait;
+    public int startTime;
+    
+    public int enemiesSpawned;
+    public int maxEnemies;
+    
+    public bool halt;
+    void Start()
+    {
+        StartCoroutine(waitSpawner());
+    }
+
+    void Update()
+    {
+        spawnWait = Random.Range(spawnMinWait, spawnMaxWait);
+        enemiesSpawned++;
+    }
+
+
+    IEnumerator waitSpawner()
+    {
+        yield return new WaitForSeconds(startTime);
+        while (!halt && maxEnemies < enemiesSpawned)
+        {
+            int randEnemy = Random.Range(0, enemies.Length);
+            
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnLimit.x, spawnLimit.x),
+                                            Random.Range(-spawnLimit.z, spawnLimit.z));
+            
+            Instantiate(enemies[randEnemy], spawnPos + transform.TransformPoint(0, 0, 0),
+                            gameObject.transform.rotation);
+
+            yield return new WaitForSeconds(spawnWait);
+        }
+    }
+}
+
