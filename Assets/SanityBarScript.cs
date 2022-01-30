e2bae13da8b16ece55ecd89bf7f5d2fea2c00111
Stormyrice <56242638+Stormyrice@users.noreply.github.com>
2022-01-30 00:09:15 +0000

    added sanityBarScript

--- Assets/SanityBarScript.cs
+++ Assets/SanityBarScript.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class SanityBarScript : MonoBehaviour
 {
     public Transform pivot;
+    public playScript playerScript;
+    private float tmp;
+    private float tmp_rotation;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerScript = GameObject.Find("Player").GetComponent<playScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        tmp_rotation = 0.55f * playerScript.sanityVar;
+        //transform.SetPositionAndRotation(transform.position, Quaternion.AngleAxis());
     }
 }
